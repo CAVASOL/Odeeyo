18041eec81a90cc10a955d7cef94682dac915637
Yeon Kim <awyeon@gmail.com>
2022-12-25 02:30:37 +0800

    added

--- frontend/src/pages/NewTicket.jsx
+++ frontend/src/pages/NewTicket.jsx
@@ -4,19 +4,21 @@ import { useNavigate } from 'react-router-dom'
 import { toast } from 'react-toastify'
 import { createTicket, reset } from '../features/tickets/ticketSlice'
 import Spinner from '../components/Spinner'
+import BackButton from '../components/BackButton'
 
 function NewTicket() {
-  const { user } = useSelector((state) => state.user)
+  const { user } = useSelector((state) => state.auth)
   const { isLoading, isError, isSuccess, message } = useSelector(
-    (state) => state.ticket
+    (state) => state.tickets
   )
+
   const [name] = useState(user.name)
   const [email] = useState(user.email)
-  const [product, setProduct] = useState('Magic Copybook Kit')
+  const [product, setProduct] = useState('')
   const [description, setDescription] = useState('')
 
-  const navigate = useNavigate()
   const dispatch = useDispatch()
+  const navigate = useNavigate()
 
   useEffect(() => {
     if (isError) {
@@ -42,6 +44,7 @@ function NewTicket() {
 
   return (
     <>
+      <BackButton url='/' />
       <section className='heading'>
         <h1>Create New Ticket</h1>
         <p>Please fill out the form below</p>
